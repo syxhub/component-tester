66555ed9b4aabf4a3fd6b108f575d51ae6daf7e4
Miki | FrontendDev | Wanari <Miki@wanari.com>
2017-11-03 14:53:28 +0100

    CT-002: add calendar/datepicker to dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,24 @@
 import { AgmCoreModule } from '@agm/core';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatDatepickerModule, MatNativeDateModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgDraggableModule } from 'angular-draggable';
 import { DragulaModule } from 'ng2-dragula';
 
+import { LoginComponent } from './account/login/login.component';
 import { SignUpComponent } from './account/sign-up/sign-up.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarouselComponent } from './layout/carousel/carousel.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
-import { LoginComponent } from './account/login/login.component';
-import { DashboardComponent } from './subpages/dashboard/dashboard.component';
+import { DatepickerComponent } from './subpages/dashboard/dashboard-components/datepicker/datepicker.component';
 import { TaskContainerComponent } from './subpages/dashboard/dashboard-components/task-container/task-container.component';
 import { TaskComponent } from './subpages/dashboard/dashboard-components/task-container/task/task.component';
+import { DashboardComponent } from './subpages/dashboard/dashboard.component';
 import { SettingsComponent } from './subpages/settings/settings.component';
 import { MapComponent } from './subpages/support/map/map.component';
 import { SupportComponent } from './subpages/support/support.component';
@@ -33,13 +36,17 @@ import { SupportComponent } from './subpages/support/support.component';
     MapComponent,
     SignUpComponent,
     SettingsComponent,
-    CarouselComponent
+    CarouselComponent,
+    DatepickerComponent
   ],
   imports: [
     AppRoutingModule,
     BrowserAnimationsModule,
     BrowserModule,
     DragulaModule,
+    FormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
     NgDraggableModule,
     NgbModule.forRoot(),
     AgmCoreModule.forRoot({
